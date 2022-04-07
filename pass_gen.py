0f1bfa6247d3de19efe53866c4d90e61a32a1959
wanghan <1357910419@qq.com>
2022-04-07 20:54:44 +0800

    直接生成符合要求的密码

--- pass_gen.py
+++ pass_gen.py
@@ -38,11 +38,25 @@ def evaluate_password(password,show_info=True):
     return result
 
 def generate_password():
-    all_char__set=string.ascii_lowercase\
-                  +string.ascii_uppercase\
-                  +string.digits+string.punctuation
+    all_char__set=string.printable[:-6]
     all_char__set *= 9
     result = ' '.join(random.sample(all_char__set,k=9))
+    return result
+
+def create_password(pass_length,confuse=False):
+    result = ''
+    #先生成包含四类字符的前四位密码
+    result += random.choice(string.ascii_uppercase)
+    result += random.choice(string.ascii_lowercase)
+    result += random.choice(string.digits)
+    result += random.choice(string.punctuation)
+    if confuse:
+        result += 'Il'
+        result += ''.join(random.sample(string.printable[:-6] * pass_length, pass_length - 6))
+    else:
+        result += ''.join(random.sample(string.printable[:-6]*pass_length,pass_length-4))
+    #对生成密码进行随机打乱
+    result = ''.join(random.sample(result,len(result)))
     return result
 
 def main_userinput():
@@ -50,12 +64,17 @@ def main_userinput():
         user_password = input('请输入新密码!')
         if evaluate_password(user_password):
             break
-def main():
+
+def main_genpassword():
     while 1:
         user_password = generate_password()
         if evaluate_password(user_password,show_info=False):
             print(f"新生成密码为：{user_password}")
             break
+
+def main():
+    for i in range(1):
+       print(f"新生成密码为:{create_password(9,True)}")
 
 main()
 
